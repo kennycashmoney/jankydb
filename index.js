b7f2556840f9b7a20ddae2e13a4246c3df5c442b
kennycashmoney <ken.larose@proton.me>
2023-09-24 20:38:30 -0500

    readAll returns a JSON object (instead of string)

--- index.js
+++ index.js
@@ -26,8 +26,7 @@ const createDb = (dbName) => {
  * @param {object} newCustomObject - the custom object to insert into the database
  */
 const createRecord = (dbName, newCustomObject) => {
-  const db = readAll(dbName);
-  const data = JSON.parse(db);
+  const data = readAll(dbName);
   const newRecord = {
     id: data.nextAutoId++,
     createdAt: new Date(),
@@ -42,21 +41,13 @@ const createRecord = (dbName, newCustomObject) => {
   console.log(`New record created: ${JSON.stringify(newRecord)}`);
 };
 
-
-
-// TODO: read a record - args: the id of the record, or just get all...
-// const readRecord()
-// update a record
-// delete a record
-
 /**
  * Destroys a record in the database
  * @param {string} dbName - the name of the database to insert to
  * @param {*} recordId - the id of the record to be destroyed
  */
 const destroyRecord = (dbName, recordId) => {
-  const db = readAll(dbName);
-  const data = JSON.parse(db);
+  const data = readAll(dbName);
 
   const updatedRecords = data.records.filter((record) => record.id !== recordId);
   data.records = updatedRecords;
@@ -67,14 +58,14 @@ const destroyRecord = (dbName, recordId) => {
   console.log(`record ${recordId} destroyed`);
 }
 
-//* bulk operations */
-// create many new records
-// read many or all records
+/**
+ * 
+ * @param {string} dbName - the name of the database to read from
+ * @returns {object} - the full database JSON
+ */
 const readAll = (dbName) => {
-  return fs.readFileSync(dbName, { encoding: "utf8" });
+  return JSON.parse(fs.readFileSync(dbName, { encoding: "utf8" }));
 };
-// update many or all records
-// delete many or all records
 
 
 // CommonJS export
@@ -84,19 +75,3 @@ module.exports = {
   destroyRecord,
   readAll,
 }
-
-
-/**
- * TODOS:
- * maybe i should group nextautoID in an object about the db itself...
- * to keep it seperated from the 'columns'
- * 
- * how can i flatten out the object schema so the 'columns' sit as siblings?
- * 
- * should i transform this thing into an object? probably...
- * i need to think about how to export this thing to be used in other applications
- * so - do i make this a CommonJS module or an ES Module? - 
- * --if i use typescript, i think it will want ES Module...
- * 
- * i'm using synchronous filesystem functions, but should i create asyncronous versions? 
- */
